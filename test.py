750ba35275a0a5c50f17424a87552efbff884338
danielmulroy <46675810+danielmulroy@users.noreply.github.com>
2019-01-14 12:05:52 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+def makeSquare(w,h):
+    top = chr(9484)
+    bottom = chr(9492)
+    row = chr(124)
+    for i in range(0,w-2):
+        top = top + "-"
+        bottom = bottom + "-"
+        row = row + " "
+    top = top + chr(9488)
+    bottom = bottom + chr(9496)
+    row = row + chr(124)
+    print(top)
+    for i in range(0,h-2):
+        print(row)
+    print(bottom)
+
+wid = int(input("What is the width? (minimum 2) "))
+hei = int(input("What is the height? (minimum 2) "))
+
+makeSquare(wid,hei)
+
+# ┌ is 9484
+# └ is 9492
+# ┘ is 9496
+# ┐ is 9488
+# | is 124
